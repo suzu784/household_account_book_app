2ada9740c09f4e39df3d22b2cb76b95fda51338f
suzu784 <s.suzuki784@hotmail.com>
2023-10-20 17:32:03 +0900

    add routing

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,21 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
-use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Application;
+use App\Http\Controllers\BudgetController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ReportController;
+use App\Http\Controllers\TransactionController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::resource('/transactions', TransactionController::class)
+    ->middleware('auth');
+
+Route::resource('/reports', ReportController::class)
+->middleware('auth');
+
+Route::resource('/budgets', BudgetController::class)
+    ->middleware('auth');
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
