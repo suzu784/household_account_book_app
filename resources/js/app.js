9c63813335ebd1f710461dddd92fdc7c7923fe25
suzu784 <s.suzuki784@hotmail.com>
2023-12-15 19:32:08 +0900

    Reintroduction of Vuetify

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,8 +5,10 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from "@inertiajs/inertia-vue3";
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import { createVuetify } from 'vuetify';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const vuetify = createVuetify();
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
@@ -15,6 +17,7 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue, Ziggy)
+            .use(vuetify)
             .mount(el);
     },
     progress: {
